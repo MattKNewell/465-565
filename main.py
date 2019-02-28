602976d78b40c40a49dda5bd52fefaf09e12fd3f
Matthew Newell <MatthewKNewell@gmail.com>
2019-02-28 15:23:41 -0800

    Add a function in order to get vision api

--- main.py
+++ main.py
@@ -56,5 +56,50 @@ def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 # [END app]
+
+
+
+def run_quickstart():
+    # [START vision_quickstart]
+    import io
+    import os
+
+    # Imports the Google Cloud client library
+    # [START vision_python_migration_import]
+    from google.cloud import vision
+    from google.cloud.vision import types
+    # [END vision_python_migration_import]
+
+    # Instantiates a client
+    # [START vision_python_migration_client]
+    client = vision.ImageAnnotatorClient()
+    # [END vision_python_migration_client]
+
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'resources/wakeupcat.jpg')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    #for label in labels:
+        #print(label.description)
+    # [END vision_quickstart]
+    return labels
+
+
+#if __name__ == '__main__':
+    #run_quickstart()
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080,debug=True)
