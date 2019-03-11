62c134b457860d866692b0104dcb417d6d3258d8
Danniel Sotelo <dsotelo@pdx.edu>
2019-03-11 14:23:14 -0700

    fixed duplicate broken images and cleaned up code

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # [START app]
 import logging
 import random
@@ -44,12 +30,8 @@ def hotdog():
 @app.route('/taggame')
 def taggame():
     imageToTag = "https://picsum.photos/400/400?image=" + checkImage()
-    # Labels = detect_labels_uri(imageToTag)
-    # descriptions = []
-    # for label in Labels:
-    #     descriptions.append(label.description)
     promptData = detect_labels_uri(imageToTag)
-    return render_template('tagGame.html', imageToTag=imageToTag, promptData=promptData) 
+    return render_template('tagGame.html', imageToTag=imageToTag, promptData=promptData)
 
 @app.route('/tag-result', methods=['POST'])
 def tagging():
@@ -67,30 +49,31 @@ def game():
     selection2 = checkImage()
     selection3 = checkImage()
     selection4 = checkImage()
-    selection5 = checkImage()
-    selection6 = checkImage()
-    selection7 = checkImage()
-    selection8 = checkImage()
-    selection9 = checkImage()
+   # selection5 = checkImage()
+   # selection6 = checkImage()
+   # selection7 = checkImage()
+   # selection8 = checkImage()
+   # selection9 = checkImage()
+
+    selectArray = ([selection1, selection2, selection3, selection4])# selection5, selection6,selection7, selection8, selection9])
 
     image1 = "https://picsum.photos/400/400?image=" + selection1
     image2 = "https://picsum.photos/400/400?image=" + selection2
     image3 = "https://picsum.photos/400/400?image=" + selection3
     image4 = "https://picsum.photos/400/400?image=" + selection4
-    image5 = "https://picsum.photos/400/400?image=" + selection5
-    image6 = "https://picsum.photos/400/400?image=" + selection6
-    image7 = "https://picsum.photos/400/400?image=" + selection7
-    image8 = "https://picsum.photos/400/400?image=" + selection8
-    image9 = "https://picsum.photos/400/400?image=" + selection9
+    #image5 = "https://picsum.photos/400/400?image=" + selection5
+    #image6 = "https://picsum.photos/400/400?image=" + selection6
+    #image7 = "https://picsum.photos/400/400?image=" + selection7
+    #image8 = "https://picsum.photos/400/400?image=" + selection8
+    #image9 = "https://picsum.photos/400/400?image=" + selection9
 
-    images = ([image1, image2, image3, image4, image5, image6,image7,image8,image9])
-    selectArray = ([selection1, selection2, selection3, selection4, selection5, selection6,selection7, selection8, selection9])
-    randomIndex = random.randint(1,9)
+    images = ([image1, image2, image3, image4])#, image5, image6,image7,image8,image9])
+    randomIndex = random.randint(1,4)
     correctAnswer = selectArray[randomIndex-1]
 
     # this is the data we will prompt the user with so they can made an educated guess
     promptData = detect_labels_uri("https://picsum.photos/800/800?image=" + correctAnswer)
- 
+
     return render_template('game.html', promptData=promptData, correctAnswer=correctAnswer, images=images)
 # [END form]
 
@@ -108,12 +91,7 @@ def submitted_form():
     if userGuess == correctAnswer:
         successOrFailure = "Correct"
 
-    #userGuess = "https://storage.googleapis.com/image_guess_game/" + userGuess
-    # [END submitted]
-    # [START render_template]
     return render_template('submitted_form.html', imgObject=imgObject, userGuess=userGuess, successOrFailure=successOrFailure, correctAnswer=correctAnswer)
-    # [END render_template]
-
 
 @app.errorhandler(500)
 def server_error(e):
@@ -121,45 +99,6 @@ def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 # [END app]
-
-
-def run_quickstart(image_name):
-    # [START vision_quickstart]
-    import io
-    import os
-
-    # Imports the Google Cloud client library
-    # [START vision_python_migration_import]
-    from google.cloud import vision
-    from google.cloud.vision import types
-    # [END vision_python_migration_import]
-
-    # Instantiates a client
-    # [START vision_python_migration_client]
-    client = vision.ImageAnnotatorClient()
-    # [END vision_python_migration_client]
-
-    # The name of the image file to annotate
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        "resources/" + image_name)
-
-    # Loads the image into memory
-    with io.open(file_name, 'rb') as image_file:
-        content = image_file.read()
-
-    image = types.Image(content=content)
-
-    # Performs label detection on the image file
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
-
-    #print('Labels:')
-    #for label in labels:
-        #print(label.description)
-     #[END vision_quickstart]
-    return labels
-
 
 
 def detect_labels_uri(uri):
@@ -178,26 +117,14 @@ def detect_labels_uri(uri):
     client = vision.ImageAnnotatorClient()
     # [END vision_python_migration_client]
 
-    # The name of the image file to annotate
-    #file_name = os.path.join(
-        #os.path.dirname(__file__),
-        #"resources/" + image_name)
     image = vision.types.Image()
     image.source.image_uri = uri
-    # Loads the image into memory
-    #with io.open(file_name, 'rb') as image_file:
-        #content = image_file.read()
 
-    #image = types.Image(content=content)
 
     # Performs label detection on the image file
     response = client.label_detection(image=image)
     labels = response.label_annotations
 
-    #print('Labels:')
-    #for label in labels:
-        #print(label.description)
-     #[END vision_quickstart]
     return labels
 
 def getRandomNumber():
@@ -208,7 +135,7 @@ def checkImage():
     rando1 = getRandomNumber()
     ranUrl = detect_labels_uri("https://picsum.photos/800/800?image=" + rando1)
     if not ranUrl:
-      return "0"
+      return checkImage()
     return str(rando1)
 
 
